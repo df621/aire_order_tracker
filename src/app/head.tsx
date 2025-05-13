c4228ffb60ae9c10f03f6e90c2b78f10e5d3e0f4
df621 <df621@ic.ac.uk>
2025-05-13 19:42:03 +0100

    fixing favicon 3

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,15 +1,13 @@
 // app/head.tsx
-import Head from 'next/head';
-
-export default function CustomHead() {
+export default function Head() {
   return (
-    <Head>
+    <>
       <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
       <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
       <link rel="shortcut icon" href="/favicon.ico" />
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
       <meta name="apple-mobile-web-app-title" content="Tracker" />
       <link rel="manifest" href="/site.webmanifest" />
-    </Head>
+    </>
   );
 }
