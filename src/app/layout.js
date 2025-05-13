6b549d7e2b5a892c16eb232718516ca66ba75598
df621 <df621@ic.ac.uk>
2025-05-13 19:33:24 +0100

    fixing favicon 2

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,5 @@
 import './globals.css';
 
-// app/layout.js (or wherever you define <head>)
-export const metadata = {
-  title: 'Aire | Pedidos',
-  icons: {
-    icon: '/favicon.png', 
-  },
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
