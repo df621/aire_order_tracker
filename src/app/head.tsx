8f7ecd47da73e8bea44c597689c87fb9827a049b
df621 <df621@ic.ac.uk>
2025-05-13 19:55:16 +0100

    fixing favicon 5

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,13 +1,10 @@
-// app/head.tsx
-export default function Head() {
-  return (
-    <>
-      <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
-      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-      <link rel="shortcut icon" href="/favicon.ico" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-      <meta name="apple-mobile-web-app-title" content="Tracker" />
-      <link rel="manifest" href="/site.webmanifest" />
-    </>
-  );
+export default function Head(){
+  <>
+    <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <meta name="apple-mobile-web-app-title" content="Tracker" />
+    <link rel="manifest" href="/site.webmanifest" />
+  </>
 }
